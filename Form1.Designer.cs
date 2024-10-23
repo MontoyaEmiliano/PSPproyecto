cb572ec61b15306e9a5aaee6ed8a9a43a91a10ef
EmilianoMontoya12 <emilianomontoya56@gmai.com>
2024-10-23 12:04:41 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace PSPproyecto
+{
+    partial class frmEmployee
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpPersonalData = new System.Windows.Forms.GroupBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblClassName = new System.Windows.Forms.TextBox();
+            this.txtClassName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.bttnSave = new System.Windows.Forms.Button();
+            this.bttnUpdate = new System.Windows.Forms.Button();
+            this.bttnDelete = new System.Windows.Forms.Button();
+            this.bttnCancel = new System.Windows.Forms.Button();
+            this.bttnInsert = new System.Windows.Forms.Button();
+            this.bttnLoad = new System.Windows.Forms.Button();
+            this.pctPhoto = new System.Windows.Forms.PictureBox();
+            this.grpPersonalData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpPersonalData
+            // 
+            this.grpPersonalData.Controls.Add(this.pctPhoto);
+            this.grpPersonalData.Controls.Add(this.bttnLoad);
+            this.grpPersonalData.Controls.Add(this.bttnInsert);
+            this.grpPersonalData.Controls.Add(this.bttnCancel);
+            this.grpPersonalData.Controls.Add(this.bttnDelete);
+            this.grpPersonalData.Controls.Add(this.bttnUpdate);
+            this.grpPersonalData.Controls.Add(this.bttnSave);
+            this.grpPersonalData.Controls.Add(this.txtAdress);
+            this.grpPersonalData.Controls.Add(this.lblAdress);
+            this.grpPersonalData.Controls.Add(this.txtPhoneNumber);
+            this.grpPersonalData.Controls.Add(this.lblPhoneNumber);
+            this.grpPersonalData.Controls.Add(this.txtEmail);
+            this.grpPersonalData.Controls.Add(this.lblEmail);
+            this.grpPersonalData.Controls.Add(this.lblClassName);
+            this.grpPersonalData.Controls.Add(this.txtClassName);
+            this.grpPersonalData.Controls.Add(this.txtName);
+            this.grpPersonalData.Controls.Add(this.lblName);
+            this.grpPersonalData.Location = new System.Drawing.Point(27, 12);
+            this.grpPersonalData.Name = "grpPersonalData";
+            this.grpPersonalData.Size = new System.Drawing.Size(704, 407);
+            this.grpPersonalData.TabIndex = 0;
+            this.grpPersonalData.TabStop = false;
+            this.grpPersonalData.Text = "Datos personales";
+            this.grpPersonalData.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(100, 234);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(219, 22);
+            this.txtAdress.TabIndex = 10;
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Location = new System.Drawing.Point(32, 240);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(64, 16);
+            this.lblAdress.TabIndex = 9;
+            this.lblAdress.Text = "Dirección";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(100, 176);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(219, 22);
+            this.txtPhoneNumber.TabIndex = 8;
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(32, 182);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(61, 16);
+            this.lblPhoneNumber.TabIndex = 7;
+            this.lblPhoneNumber.Text = "Telefono";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(100, 120);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(219, 22);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(32, 126);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(41, 16);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.Location = new System.Drawing.Point(100, 69);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(219, 22);
+            this.lblClassName.TabIndex = 4;
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.AutoSize = true;
+            this.txtClassName.Location = new System.Drawing.Point(32, 72);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(64, 16);
+            this.txtClassName.TabIndex = 3;
+            this.txtClassName.Text = "Apellidos";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(100, 21);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(219, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(32, 27);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(56, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nombre";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bttnSave
+            // 
+            this.bttnSave.Location = new System.Drawing.Point(35, 344);
+            this.bttnSave.Name = "bttnSave";
+            this.bttnSave.Size = new System.Drawing.Size(75, 23);
+            this.bttnSave.TabIndex = 11;
+            this.bttnSave.Text = "Guardar";
+            this.bttnSave.UseVisualStyleBackColor = true;
+            // 
+            // bttnUpdate
+            // 
+            this.bttnUpdate.Location = new System.Drawing.Point(139, 344);
+            this.bttnUpdate.Name = "bttnUpdate";
+            this.bttnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.bttnUpdate.TabIndex = 12;
+            this.bttnUpdate.Text = "Modificar";
+            this.bttnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // bttnDelete
+            // 
+            this.bttnDelete.Location = new System.Drawing.Point(244, 344);
+            this.bttnDelete.Name = "bttnDelete";
+            this.bttnDelete.Size = new System.Drawing.Size(75, 23);
+            this.bttnDelete.TabIndex = 13;
+            this.bttnDelete.Text = "Eliminar";
+            this.bttnDelete.UseVisualStyleBackColor = true;
+            // 
+            // bttnCancel
+            // 
+            this.bttnCancel.Location = new System.Drawing.Point(349, 344);
+            this.bttnCancel.Name = "bttnCancel";
+            this.bttnCancel.Size = new System.Drawing.Size(75, 23);
+            this.bttnCancel.TabIndex = 14;
+            this.bttnCancel.Text = "Cancelar";
+            this.bttnCancel.UseVisualStyleBackColor = true;
+            // 
+            // bttnInsert
+            // 
+            this.bttnInsert.Location = new System.Drawing.Point(367, 99);
+            this.bttnInsert.Name = "bttnInsert";
+            this.bttnInsert.Size = new System.Drawing.Size(75, 23);
+            this.bttnInsert.TabIndex = 15;
+            this.bttnInsert.Text = "Nuevo";
+            this.bttnInsert.UseVisualStyleBackColor = true;
+            // 
+            // bttnLoad
+            // 
+            this.bttnLoad.Location = new System.Drawing.Point(367, 201);
+            this.bttnLoad.Name = "bttnLoad";
+            this.bttnLoad.Size = new System.Drawing.Size(75, 24);
+            this.bttnLoad.TabIndex = 16;
+            this.bttnLoad.Text = "Cargar";
+            this.bttnLoad.UseVisualStyleBackColor = true;
+            // 
+            // pctPhoto
+            // 
+            this.pctPhoto.Location = new System.Drawing.Point(497, 99);
+            this.pctPhoto.Name = "pctPhoto";
+            this.pctPhoto.Size = new System.Drawing.Size(162, 126);
+            this.pctPhoto.TabIndex = 17;
+            this.pctPhoto.TabStop = false;
+            this.pctPhoto.Click += new System.EventHandler(this.pctPhoto_Click);
+            // 
+            // frmEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpPersonalData);
+            this.Name = "frmEmployee";
+            this.Text = "Empleados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpPersonalData.ResumeLayout(false);
+            this.grpPersonalData.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpPersonalData;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox lblClassName;
+        private System.Windows.Forms.Label txtClassName;
+        private System.Windows.Forms.Button bttnCancel;
+        private System.Windows.Forms.Button bttnDelete;
+        private System.Windows.Forms.Button bttnUpdate;
+        private System.Windows.Forms.Button bttnSave;
+        private System.Windows.Forms.Button bttnInsert;
+        private System.Windows.Forms.PictureBox pctPhoto;
+        private System.Windows.Forms.Button bttnLoad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSPproyecto
+{
+    public partial class frmEmployee : Form
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctPhoto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adress { get; set; }
+        public string Photo { get; set; }
+    }
+
+}
